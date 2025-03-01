3549d49b936333391cc9c8a3d36cfd2f4bdccfd3
nathvincil <essoussena@gmail.com>
2025-03-01 03:02:52 +0000

    initialpush

---
+++ script.js
@@ -0,0 +1,33 @@
+
+let socket;
+
+// Connect to the WebSocket server running on the Raspberry Pi Pico W
+function connectWebSocket() {
+    socket = new WebSocket('ws://your_pico_w_ip:81');  // Change this IP to your Pico W's IP
+    
+    socket.onopen = () => {
+        console.log('Connected to Raspberry Pi Pico W');
+    };
+
+    socket.onmessage = (event) => {
+        console.log('Message from Pico W: ' + event.data);
+    };
+
+    socket.onerror = (error) => {
+        console.log('WebSocket error: ' + error);
+    };
+}
+
+// Send message to turn LED on
+function turnLEDOn() {
+    if (socket && socket.readyState === WebSocket.OPEN) {
+        socket.send('LED_ON');
+    }
+}
+
+// Send message to turn LED off
+function turnLEDOff() {
+    if (socket && socket.readyState === WebSocket.OPEN) {
+        socket.send('LED_OFF');
+    }
+}
