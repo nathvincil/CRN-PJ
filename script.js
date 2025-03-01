f86fc3b5d6c34d36502bed052d3937df44a11e0c
nathvincil <essoussena@gmail.com>
2025-03-01 03:36:43 +0000

    final

--- script.js
+++ script.js
@@ -1,43 +1,43 @@
 
-let socket;
+        let socket;
 
-// Simple UUID generator (no crypto API used)
-function generateUUID() {
-    return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
-        var r = Math.random() * 16 | 0, v = c === 'x' ? r : (r & 0x3 | 0x8);
-        return v.toString(16);
-    });
-}
+        // Simple UUID generator (no crypto API used)
+        function generateUUID() {
+            return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+                var r = Math.random() * 16 | 0, v = c === 'x' ? r : (r & 0x3 | 0x8);
+                return v.toString(16);
+            });
+        }
 
-// WebSocket connection function
-function connectWebSocket() {
-    socket = new WebSocket('ws://your_pico_w_ip:81');  // Replace with your Pico W's IP
+        // WebSocket connection function
+        function connectWebSocket() {
+            socket = new WebSocket('ws:192.168.6.126:80');  // Replace with your Pico W's IP
 
-    socket.onopen = () => {
-        console.log('Connected to Raspberry Pi Pico W');
-    };
+            socket.onopen = () => {
+                console.log('Connected to Raspberry Pi Pico W');
+            };
 
-    socket.onmessage = (event) => {
-        console.log('Message from Pico W: ' + event.data);
-    };
+            socket.onmessage = (event) => {
+                console.log('Message from Pico W: ' + event.data);
+            };
 
-    socket.onerror = (error) => {
-        console.log('WebSocket error: ' + error);
-    };
-}
+            socket.onerror = (error) => {
+                console.log('WebSocket error: ' + error);
+            };
+        }
 
-// Function to turn LED on
-function turnLEDOn() {
-    console.log("Turning LED On");
-    if (socket && socket.readyState === WebSocket.OPEN) {
-        socket.send('LED_ON');
-    }
-}
+        // Function to turn LED on
+        function turnLEDOn() {
+            console.log("Turning LED On");
+            if (socket && socket.readyState === WebSocket.OPEN) {
+                socket.send('LED_ON');
+            }
+        }
 
-// Function to turn LED off
-function turnLEDOff() {
-    console.log("Turning LED Off");
-    if (socket && socket.readyState === WebSocket.OPEN) {
-        socket.send('LED_OFF');
-    }
-}
+        // Function to turn LED off
+        function turnLEDOff() {
+            console.log("Turning LED Off");
+            if (socket && socket.readyState === WebSocket.OPEN) {
+                socket.send('LED_OFF');
+            }
+        }
